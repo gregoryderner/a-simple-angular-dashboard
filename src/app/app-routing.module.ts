75639b12a3f8d47d40269694b03ee0180200ca3c
Grégory Derner <gregoryderner@gmail.com>
2024-07-16 11:56:43 -0300

    fix duplicated router-outlet and remove routes from feature module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 
 const routes: Routes = [
   {
@@ -8,18 +9,29 @@ const routes: Routes = [
     loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule)
   },
   {
-    path: 'clients',
-    loadChildren: () => import('./features/clients/clients.module').then(m => m.ClientsModule),
-    canActivate: [AuthGuard]
-  },
-  {
     path: '',
-    redirectTo: 'clients',
-    pathMatch: 'full'
-  },
-  {
-    path: '**',
-    redirectTo: 'clients'
+    component: MainLayoutComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'clients',
+        loadChildren: () => import('./features/clients/clients.module').then(m => m.ClientsModule)
+      },
+      /* {
+        path: 'users',
+        loadChildren: () => import('./features/users/users.module').then(m => m.UsersModule),
+        canActivate: [AuthGuard]
+      }, */
+      {
+        path: '',
+        redirectTo: 'clients',
+        pathMatch: 'full'
+      },
+      {
+        path: '**',
+        redirectTo: 'clients'
+      }
+    ]
   }
 ];
 
--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,24 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainLayoutComponent } from '../shared/components/main-layout/main-layout.component';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: MainLayoutComponent,
-    children: [
-      { path: 'clients', loadChildren: () => import('./clients/clients.module').then(m => m.ClientsModule) },
-      { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }
-    ]
-  }
-];
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule
   ],
   exports: [RouterModule]
 })
